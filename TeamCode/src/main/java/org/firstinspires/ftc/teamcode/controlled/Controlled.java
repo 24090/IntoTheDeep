8289e5d1e5e1053619d125932636605c0d60ec18
hmoose-ash <142426250+hmoose-ash@users.noreply.github.com>
2024-08-27 22:09:28 +0200

    Basic Controlled mode
    Swaps out mecanum drive localization for a the 3-encoder version, uses it for an opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controlled/Controlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controlled/Controlled.java
@@ -1,13 +1,27 @@
 package org.firstinspires.ftc.teamcode.controlled;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.roadrunner.drive.StandardTrackingWheelLocalizer;
-import java.util.List;
-@TeleOp
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+/**
+ * TODO:
+ *  give pose to next opmode/take pose from previous
+ *  assisted aim if necessary
+ *  dpad mode if necessary
+ */
+@TeleOp(name = "Controller")
 public class Controlled extends LinearOpMode{
     public void runOpMode(){
-
-        StandardTrackingWheelLocalizer localizer = new StandardTrackingWheelLocalizer(hardwareMap,StandardTrackingWheelLocalizer.getWheelPositions() , );
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0.0, 0.0, 0.0));
+        while (opModeIsActive()){
+            // y and x ARE correct here, as "forward" is represented by the X coordinate for roadrunner and the Y coordinate on the controller
+            Vector2d linearVelocity = new Vector2d(gamepad1.right_stick_y, gamepad1.right_stick_x);
+            double angularVelocity = gamepad1.right_trigger - gamepad1.left_trigger;
+            drive.setDrivePowers(new PoseVelocity2d(linearVelocity, angularVelocity));
+        }
     }
-
 }
