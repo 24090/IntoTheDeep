4bd16b9433e3846ded3829430b447b18c5635cec
hmoose-ash <142426250+hmoose-ash@users.noreply.github.com>
2024-09-07 21:27:29 +0200

    Add INTO THE DEEP image for MeepMeep
    This can be undone once MeepMeep has them by default

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -5,8 +5,15 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
+
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
@@ -15,17 +22,18 @@ public class MeepMeepTesting {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .build());
-
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .splineTo(GameMap.NetRedCorner, 0)
+                .build()
+        );
+        Image image = null;
+        try {
+            File image_file = new File("./MeepMeepTesting/src/main/java/com/example/meepmeeptesting/ftc-map.png");
+            image = ImageIO.read(image_file);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        assert image != null;
+        meepMeep.setBackground(image)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
