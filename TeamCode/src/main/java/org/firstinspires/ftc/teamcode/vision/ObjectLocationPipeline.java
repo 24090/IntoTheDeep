ff68231972e9fa7b110bc4d4ac909e76fead5a07
hmoose-ash <142426250+hmoose-ash@users.noreply.github.com>
2024-12-03 16:03:54 +0100

    Theoretically working
    Transformation Matrix ≠ camera transformation matrix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/ObjectLocationPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/ObjectLocationPipeline.java
@@ -1,17 +1,13 @@
 package org.firstinspires.ftc.teamcode.vision;
-import static java.util.Arrays.asList;
 
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfKeyPoint;
-import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.features2d.Features2d;
 import org.opencv.features2d.SimpleBlobDetector;
 import org.opencv.features2d.SimpleBlobDetector_Params;
@@ -19,11 +15,8 @@ import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
-import java.util.stream.Stream;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -31,7 +24,6 @@ public class ObjectLocationPipeline extends OpenCvPipeline {
     Telemetry telemetry;
     public Scalar color_min;
     public Scalar color_max;
-
     public int stage = 4;
     public Mat input_undistort = new Mat();
     public Mat input_hsv  = new Mat();
@@ -40,29 +32,30 @@ public class ObjectLocationPipeline extends OpenCvPipeline {
     public Mat keypoints_drawn_image = new Mat();
     public Mat output = new Mat();
     Mat empty_mat = new Mat();
-    Mat transformation_matrix = new Mat();
-    Mat multiplication = new Mat();
+    Mat reprojection_matrix = new Mat();
     public Vector<Double>[] object_field_coords = new Vector[]{};
-    public ObjectLocationPipeline(CameraInfo.Colors color, Telemetry telemetry) {
+    public ObjectLocationPipeline(Camera.Colors color, Telemetry telemetry) {
+        this.reprojection_matrix = Camera.getReprojectionMatrix();
         this.telemetry = telemetry;
         switch (color){
             case RED:
-                this.color_min = CameraInfo.red_min;
-                this.color_max = CameraInfo.red_max;
+                this.color_min = Camera.red_min;
+                this.color_max = Camera.red_max;
                 break;
             case BLUE:
-                this.color_min = CameraInfo.blue_min;
-                this.color_max = CameraInfo.blue_max;
+                this.color_min = Camera.blue_min;
+                this.color_max = Camera.blue_max;
                 break;
             case YELLOW:
-                this.color_min = CameraInfo.yellow_min;
-                this.color_max = CameraInfo.yellow_max;
+                this.color_min = Camera.yellow_min;
+                this.color_max = Camera.yellow_max;
                 break;
         }
     }
     public ObjectLocationPipeline(Telemetry telemetry){
-        this.color_min = CameraInfo.yellow_min;
-        this.color_max = CameraInfo.yellow_max;
+        this.reprojection_matrix = Camera.getReprojectionMatrix();
+        this.color_min = Camera.yellow_min;
+        this.color_max = Camera.yellow_max;
         this.telemetry = telemetry;
     }
 
@@ -71,7 +64,8 @@ public class ObjectLocationPipeline extends OpenCvPipeline {
 //        keypoints_drawn_image = new Mat();
 //        output = new Mat();
         // Undistort
-        Calib3d.undistort(input, input_undistort, CameraInfo.getCameraMatrix(), CameraInfo.getDistortionCoefficients());
+        //Calib3d.undistort(input, input_undistort, Camera.getCameraMatrix(), Camera.getDistortionCoefficients());
+        input_undistort = input;
         // Input RGB -> HSV
         Imgproc.cvtColor(input_undistort, input_undistort, Imgproc.COLOR_RGB2HSV);
         Imgproc.cvtColor(input, input_hsv, Imgproc.COLOR_RGB2HSV);
@@ -79,8 +73,8 @@ public class ObjectLocationPipeline extends OpenCvPipeline {
         //Imgproc.blur(input_hsv, color_mask, new Size(3,3));
         Core.inRange(input_undistort, color_min, color_max, color_mask);
         //   Erode and dilate
-        Imgproc.erode(color_mask, color_mask, empty_mat, new Point(-1, -1), 5);
-        Imgproc.dilate(color_mask, color_mask, empty_mat, new Point(-1, -1), 10);
+        Imgproc.erode(color_mask, color_mask, empty_mat, new Point(-1, -1), 6);
+        Imgproc.dilate(color_mask, color_mask, empty_mat, new Point(-1, -1), 9);
 
         //   add color when mask is white, add white when mask is black
         Core.bitwise_and(input_undistort, input_undistort, color_filtered_image, color_mask);
@@ -114,23 +108,16 @@ public class ObjectLocationPipeline extends OpenCvPipeline {
             //      See: https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html
             //      (Z = 0)
             List<Vector<Double>> world_points = new ArrayList<>();
-            List<Mat> mats = new ArrayList<>();
-            mats.add(CameraInfo.getRotationMatrix().col(0));
-            mats.add(CameraInfo.getRotationMatrix().col(1));
-            mats.add(CameraInfo.getTranslationMatrix());
-            Core.hconcat(mats, transformation_matrix);
-            // transformation_matrix = [ R_1 | R_2 | T ]
-            Core.gemm(CameraInfo.getCameraMatrix(), transformation_matrix, 1, empty_mat, 0, multiplication);
-            multiplication = multiplication.inv();
+            // multiplication = (intrinsics * [ R_1 | R_2 | T ])^-1
             for (int i = 0; i < points.rows(); i++) {
                 Mat screen_coords = new Mat(3, 1, 6);
                 screen_coords.put(0,0, points.get(i, 0)[0]);
                 screen_coords.put(1,0, points.get(i, 0)[1]);
                 screen_coords.put(2,0, 1);
                 // screen_coords = [u, v, 1]
-                Mat result = new Mat();
-                Core.gemm(multiplication, screen_coords, 1, empty_mat, 0, result);
-                double scale = 1.0;//result.get(2,0)[0];
+                Mat result = reprojection_matrix.matMul(screen_coords);
+                // [x, y, 1] = (intrinsics * [ R_1 | R_2 | T ])^-1 * [u, v, 1] * s
+                double scale = 1.0/result.get(2,0)[0];
                 Vector<Double> result_vector = new Vector<>(3);
                 result_vector.add(result.get(0,0)[0] * scale);
                 result_vector.add(result.get(1,0)[0] * scale);
