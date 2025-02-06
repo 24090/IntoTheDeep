4f01ee236df9feeecd9c275a4f39248706a9ae30
hmoose-ash <142426250+hmoose-ash@users.noreply.github.com>
2025-02-06 15:36:24 +0100

    Create PushBot.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controlled/PushBot.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.controlled;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+@TeleOp(name = "PushBot")
+public class PushBot extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        while (opModeIsActive()) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0.0, 0.0, 0.0));
+            drive.setDrivePowers(new PoseVelocity2d(new Vector2d(-gamepad1.left_stick_y, -gamepad1.left_stick_x), -gamepad1.right_stick_x));
+        }
+}}
